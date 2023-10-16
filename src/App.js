a30dd45db9f4e949433770be499771f4295b8cd3
Simon Yoo <yoosghn0@gmail.com>
2023-10-17 11:04:26 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+    </div>
+  );
+}
+
+export default App;
